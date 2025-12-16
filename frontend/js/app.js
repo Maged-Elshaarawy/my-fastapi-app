d65bbd3e235b4d3e20a48b333eec432e117a8894
root <maged2015373@gmail.com>
2025-12-16 21:52:39 +0000

    changed app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,17 @@
-fetch("/api/health")
-  .then(res => res.json())
-  .then(data => console.log(data));
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: "Hello from Vue"
+    }
+  },
+  methods: {
+    async checkApi() {
+      const res = await fetch("/api/health");
+      const data = await res.json();
+      alert(data.status);
+    }
+  }
+}).mount("#app");
 
